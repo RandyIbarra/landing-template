89da177b60b3c127045b770b3ca207a1a0ae956c
JacobHdez <jacohv98@gmail.com>
2021-07-19 19:01:31 -0500

    Se crea landing formal con el diseño definido

---
+++ formal/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'c_01': '#f6ede0',
+        'c_02': '#ddab2a',
+        'c_03': '#86681a',
+        'c_04': '#312f2f',
+        'c_05': '#fefbf7'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
